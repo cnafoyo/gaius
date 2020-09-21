73ff139bd96cc4a48f0b39482735f0dd7a2ed6e7
Billy Chepkemoi <cnafayo@gmail.com>
2020-09-21 15:22:09 +0300

    Creating array of product

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 
 function Product(props) {
     return (
         <React.Fragment>
-            <h1>Yolomy Products</h1>
-            <p> {props.name} </p>
-            <p>Price - {props.price} </p>
+            <h4>{props.name}</h4>
+            <p>Price- {props.price} </p>
+
         </React.Fragment>
     )
+}
+Product.propTypes = {
+    name: PropTypes.string,
+    price: PropTypes.string
 }
 
 export default Product
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,11 +1,51 @@
 import React from 'react';
 import Product from './Product';
+
+const actualProductList = [
+    {
+        name: 'T-Shirt',
+        price: '599',
+        id: "1"
+    },
+    {
+        name: 'BackPack',
+        price: '1500',
+        id: "2"
+    },
+    {
+        name: 'Pants',
+        price: '1000',
+        id: '3'
+    },
+    {
+        name: 'Trekking Shoes',
+        price: '2000',
+        id: '4'
+    },
+    {
+        name: 'Jacket',
+        price: '1500',
+        id: '5'
+    },
+    {
+        name: 'T-Shirt Ladies',
+        price: '650',
+        id: '6'
+    }
+]
+
 function ProductList() {
     return (
         <React.Fragment>
-            <Product
-                name="Trousers"
-                price="1500" />
+            <h1>Yolomy Products</h1>
+            {actualProductList.map((product) => (
+                <Product
+                    name={product.name}
+                    price={product.price}
+                    key={product.id}
+                />
+            ))}
+
         </React.Fragment>
     )
 }
